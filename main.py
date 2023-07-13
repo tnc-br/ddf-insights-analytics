7ca940f187f17881533b52e0fe071cbe1200b852
Kader <kazob@google.com>
2023-07-13 18:12:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,28 +15,27 @@ def hello_firestore(cloud_event: CloudEvent) -> None:
     Args:
         cloud_event: cloud event with information on the firestore event trigger
     """
-    # firestore_payload = firestoredata.DocumentEventData()
-    # firestore_payload._pb.ParseFromString(cloud_event.data)
-    # #print(f"Received event with ID: {cloud_event['id']} and data {cloud_event.data}")
+    firestore_payload = firestoredata.DocumentEventData()
+    firestore_payload._pb.ParseFromString(cloud_event.data)
+    #print(f"Received event with ID: {cloud_event['id']} and data {cloud_event.data}")
 
-    # path_parts = firestore_payload.value.name.split("/")
-    # separator_idx = path_parts.index("documents")
-    # collection_path = path_parts[separator_idx + 1]
-    # document_path = "/".join(path_parts[(separator_idx + 2) :])
+    path_parts = firestore_payload.value.name.split("/")
+    separator_idx = path_parts.index("documents")
+    collection_path = path_parts[separator_idx + 1]
+    document_path = "/".join(path_parts[(separator_idx + 2) :])
 
-    # print(f"Collection path: {collection_path}")
-    # print(f"Document path: {document_path}")
-    # client: google.cloud.firestore.Client = firestore.client()
+    print(f"Collection path: {collection_path}")
+    print(f"Document path: {document_path}")
+    client: google.cloud.firestore.Client = firestore.client()
 
-    # affected_doc = client.collection(collection_path).document(document_path)
+    affected_doc = client.collection(collection_path).document(document_path)
 
-    # # cur_value = firestore_payload.value.fields["original"].string_value
-    # # new_value = cur_value.upper()
+    # cur_value = firestore_payload.value.fields["original"].string_value
+    # new_value = cur_value.upper()
     
-    # value = affected_doc.get().to_dict()
+    value = affected_doc.get().to_dict()
 
-    # fraud_rate = Fraud().evaluate(value)
-    # value['validity'] = fraud_rate
-    # # see https://github.com/googleapis/python-firestore/blob/main/google/cloud/firestore_v1/document.py
-    # affected_doc.set(value)
-    print("Hola fgFczd")
+    fraud_rate = 0.77 # Fraud().evaluate(value)
+    value['validity'] = fraud_rate
+    # see https://github.com/googleapis/python-firestore/blob/main/google/cloud/firestore_v1/document.py
+    affected_doc.set(value)
