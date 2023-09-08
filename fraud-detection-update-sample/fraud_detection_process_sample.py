b2c9b8e9a11c74c16e63eef1e3f55d8979d69137
Kazob1998 <kazob@google.com>
2023-09-08 14:33:10 -0400

    fix ee initialize

--- fraud-detection-update-sample/fraud_detection_process_sample.py
+++ fraud-detection-update-sample/fraud_detection_process_sample.py
@@ -3,6 +3,7 @@ import ee
 import os
 import numpy as np
 import scipy
+import google.auth
 
 """Global Variable definition"""
 
@@ -62,18 +63,8 @@ class _ttest():
         self.carbon_measurements = carbon_measurements
 
         # Initialize Earth Engine.
-        if os.path.isfile('key.json'):
-
-            # connection to the service account
-            service_account = 'fraud-detection-pipeline-sa@kazob-370920.iam.gserviceaccount.com'
-            credentials = ee.ServiceAccountCredentials(
-                service_account, 'key.json')
-            ee.Initialize(credentials)
-            print('initialized with key')
-
-        # if in local env use the local user credential
-        else:
-            ee.Initialize()
+        credentials, project_id = google.auth.default()
+        ee.Initialize(credentials)
 
         # fetching isoscapes
 
