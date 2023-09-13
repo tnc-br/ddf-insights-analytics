388c7cbf2749f2eb3067724362bb36f95bcc5cd5
Juan Castro-Varón <juan@castrovaron.com>
2023-09-13 18:31:44 +0100

    Base approach: run for given doc or just run all of them

--- fraud-detection-generate-maps/main.py
+++ fraud-detection-generate-maps/main.py
@@ -1,8 +1,27 @@
 import base64
 import functions_framework
+from firebase_admin import initialize_app, firestore
+import google.cloud.firestore
+
+app = initialize_app()
 
 # Triggered from a message on a Cloud Pub/Sub topic.
 @functions_framework.cloud_event
 def hello_pubsub(cloud_event):
     # Print out the data from Pub/Sub, to prove that it worked
-    print(str(base64.b64decode(cloud_event.data["message"]["data"])))
+    document_id = str(base64.b64decode(cloud_event.data["message"]["data"]))
+
+    client: google.cloud.firestore.Client = firestore.client()
+    collection_ref = client.collection("untrusted_samples")
+
+    # Check if the document exists
+    doc = collection_ref.document(document_id).get()
+    if doc.exists:
+        print('Document exists')
+    else:
+        print('Document does not exist')
+        # Get all documents in the collection
+        docs = collection_ref.stream()
+        # Iterate over the documents and print their IDs and data
+        for doc in docs:
+            print(f'Document ID: {doc.id}')
